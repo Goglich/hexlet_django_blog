0beb67f406929ab91a8ffb98c6ddea0e120c4824
MyName <ivant4221@gmail.com>
2025-03-16 20:29:32 +0300

    rework article_index view

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,12 +3,16 @@ from django.views import View
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import redirect
+from hexlet_django_blog.article.models import Article
+
 
 class IndexView(View):
 
-    def get(self, request):
-        name = 'article'
-        return redirect('article', tags='python', article_id=42)
+    def get(self, request, *args, **kwargs):
+       articles = Article.objects.all()[:15]
+       return render(request, 'articles/articles.html', context={
+           'articles': articles,
+       })
     
 def index(request, tags=None, article_id=None):
     return render(request, 'articles/article_index.html', context={'tags': tags, 'article_id': article_id})
